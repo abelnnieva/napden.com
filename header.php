a42d21c90fe8e5b6b031d916e8220e1729c59c5b
Abel Nieva Saavedra <abnieva@gmail.com>
2012-11-30 15:30:55 +0000

    Main page ready

---
+++ header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* The header template.
+*
+* @package    napden.com
+* @license    https://github.com/napden/napden.com/blob/master/LICENSE.md
+* @author     Abel Nieva Saavedra <abnieva@gmail.com>
+*/
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--
+                                                     nnn
+                                                     nnn
+    n  nnnnn        nnnnn  n   n  nnnnn        nnnnn nnn     nnnnn    n  nnnnn
+    nnnnnnnnnn    nnnnnnnnnn   nnnnnnnnnn    nnnnnnnnnnn   nnnnnnnnn  nnnnnnnnnn
+    nnn     nnn  nnn     nnn   nnn     nnn  nnn      nnn  nnn   nnn   nnn     nnn
+    nnn     nnn  nnn     nnn   nnn     nnn  nnn      nnn  nnn nnn     nnn     nnn
+    nnn     nnn   nnnnnnnnnn   nnnnnnnnnn    nnnnnnnnnnn   nnnnn  nn  nnn     nnn
+    nnn     nnn     nnnnn  n   nnn nnnn        nnnnnn  n     nnnnnn   nnn     nnn
+                               nnn
+                               nnn
+-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Napden</title>
+        <meta name="description" content="We are a mobile and web development agency based in Tenerife, Spain. We think, design and build amazing web apps.">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="image_src" href="apple-touch-icon-144x144-precomposed.png" />
+
+        <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-144x144-precomposed.png">
+
+        <link rel="author" href="humans.txt">
+
+        <link rel="stylesheet" href="css/main.min.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Responsive navbar -->
+        <div class="navbar navbar-static-top">
+            <div class="navbar-inner">
+                <div class="container">
+
+                    <!-- Be sure to leave the brand out there if you want it shown -->
+                    <a class="brand" href="http://napden.com" rel="tooltip" title="Inicio" data-placement="bottom">Napden</a>
+
+                    <!-- Everything you want hidden at 940px or less -->
+                    <div class="nav pull-right visible-desktop call">
+                        Call Napden now<a href="skype:napden.com?call" rel="tooltip" title="Skype" data-placement="bottom"><i class="telephone"></i></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- Header -->
+        <header class="container main-header">
+            <div class="hero-unit">
+                <h1>napden</h1>
+                <p>We are a mobile and web development agency. We think, design and build amazing web apps.</p>
+            </div>
+            <div class="row-fluid">
+                <img class="span11 offset1" src="img/landing-devices.png">
+            </div>
+        </header>
